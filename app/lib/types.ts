33668b8d16561b87983ebd76bd69d700b8297eb9
vishal-tiwari9 <vishal.urban.wheels@gmail.com>
2026-06-25 15:34:16 +0530

    Creating a interface for Market data , Holdings

---
+++ app/lib/types.ts
@@ -0,0 +1,35 @@
+
+
+export interface Holding {
+    sector :string;
+    name:string;
+    ticker:string;
+    purchasePrice:number;
+    qty:number;
+    exchange:string; // NSE or BSE 
+}
+
+export interface MArketData{
+    ticker:string;
+    cmp:number; // Current market Price 
+    peRatio:number |null;
+    latestEarnings:number |null;
+    error?:string;
+}
+
+export interface EnrichedHolding extends Holding{
+    investment:number; // purchasePrice × qty
+    portfolioPercent:number;// this holding / total investment
+    presentValue:number; // cmp × qty
+    gainloss:number;// presentValue - investment
+    gainLossPercent:number;
+    cmp:number;
+    peRatio:number|null;
+    latestEarnings:number|null;
+    isLoading?:boolean;
+    error?:string;
+
+
+
+}
+
